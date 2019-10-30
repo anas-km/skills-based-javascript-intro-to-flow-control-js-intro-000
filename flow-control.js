fcac9811a4d9f4040ecec24ea3bb4759d0c5e571
Anas K <mail.anascape@gmail.com>
2019-10-30 06:13:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,33 @@
 function basicTeenager(age) {
-
+  if (age > 13 && age < 19){
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
+  if (age > 13 && age < 19){
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
+
 
 }
 
 function ageChecker(age) {
+  if (age > 13 && age < 19){
+    return "You are a teenager!"
+  } else if (age >= 20) {
+    return "You are a grownup"
+
+  } else {
+    return "You are a kid"
+  }
 
 }
 
 function ternaryTeenager(age) {
-
+(age > 13 && age < 19) ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
