a96d49d9b76aef4d0de7acc9890bd6d86def251c
Anas K <mail.anascape@gmail.com>
2019-10-30 06:56:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,7 +31,7 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  
+
 switch (age) {
   case 13:
   return "You have an age";
@@ -64,8 +64,8 @@ switch (age) {
   default:
   return "You have an age"
 
-    
+
 }
-    
+
 }
   
